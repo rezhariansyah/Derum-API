1793472202bb30e862763001e4eb8188f207c471
Rezha <rezharatulangi@gmail.com>
2019-08-14 00:06:58 +0700

    user login and register

---
+++ app.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const xssFilter = require("x-xss-protection");
+const logger = require("morgan");
+const app = express();
+const port = process.env.SERVER_PORT || 3333;
+
+const userRoute = require("./src/routes/user");
+
+const whiteList = process.env.WHITELIST;
+
+// const corsOptions = (req, callback) => {
+//   if (whiteList.split(",").indexOf(req.header("Origin")) !== -1) {
+//     console.log("Success");
+//     return callback(null, {
+//       origin: true
+//     });
+//   } else {
+//     console.log("Failed");
+//     return callback(null, {
+//       origin: false
+//     });
+//   }
+// };
+
+app.use(cors());
+// app.options('*', cors(corsOptions))
+app.use(xssFilter())
+app.use(logger('dev'))
+
+app.listen(port, () => {
+  console.log(`\n Server is running on port : ${port} \n `);
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false })); // body type
+
+app.use('/user', userRoute);
